98566893b7b0c05027c250ab8ff6c99019b9be62
Crafsterz <30776344+Crafsterz@users.noreply.github.com>
2022-11-02 12:48:58 +0800

    Completed 1st project

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukhairu <mukhairu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 14:09:24 by mukhairu          #+#    #+#             */
+/*   Updated: 2022/10/18 12:58:32 by mukhairu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+//Allocates (with malloc) and returns a new string, which is the result
+//of the concatenation of 's1' and 's2'
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		cat;
+	char	*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(ft_strlen(s1) * sizeof(char) + ft_strlen(s2) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	cat = 0;
+	while (s1[i])
+	{
+		str[cat++] = s1[i++];
+	}
+	i = 0;
+	while (s2[i])
+	{
+		str[cat++] = s2[i++];
+	}
+	str[cat] = '\0';
+	return (str);
+}
